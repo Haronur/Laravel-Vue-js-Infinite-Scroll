3476940cf75cb12e19d3be30c3b435aa867ca7af
HARON <haronur@gmail.com>
2020-02-12 19:42:39 +0600

    Laravel-Vue-js-Infinite-Scroll_v2

---
+++ resources/js/components/InfiniteScrollComponent.vue
@@ -0,0 +1,44 @@
+<template>
+     <div class="container">
+         <div class="card" v-for="(post,index) in posts" :key="index">
+            <div class="card-body">
+                <h4 class="card-title">{{post.title}}</h4>
+                <p class="card-text">{{post.description}}</p>
+            </div>
+        </div>
+       <infinite-loading spinner="waveDots" @infinite="infiniteHandler"></infinite-loading>
+
+     </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      page: 1,
+      posts: [],
+    };
+  },
+  methods: {
+     infiniteHandler($state) {
+      setTimeout(() => {
+      axios.get('post', {
+        params: {
+          page: this.page,
+        },
+      }).then(({ data }) => {
+        if (data.data.length) {
+          this.page += 1;
+          this.posts.push(...data.data);
+          $state.loaded();
+        } else {
+          $state.complete();
+        }
+      });
+    }, 1000);
+
+    },
+  },
+};
+</script>
